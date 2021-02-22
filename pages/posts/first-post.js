bd10bfc27b654a384bdaa1c5db1f92e4fdc73e36
Dina Rocio <dinarociovq15@hotmail.com>
2021-02-22 16:45:12 -0500

    Metadata ]2;]1; has been modified

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,30 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 export default function FirstPost(){
+
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
         <h2>
           <Link href="/">Back to home</Link>
         </h2>
+        
     </>
   )
 }
+
+
+
+
+const YourComponent = () => (
+  <Image
+    src="/images/myProfile.jpg" //Ruta de la carpeta de imagen
+    height={144} // medida esperada
+    width={144}
+    alt="DinaRocio"
+  ></Image>
+)
